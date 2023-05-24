8528d385ea8ca098e1bb191d6419e58f793408b7
SM Sohag <sohagsheik32@gmail.com>
2023-05-24 20:56:25 +0600

    react helmet implemented for dynamic title on page

--- src/pages/Home/Home/Home.jsx
+++ src/pages/Home/Home/Home.jsx
@@ -1,3 +1,4 @@
+import { Helmet } from "react-helmet";
 import Banner from "../Banner";
 import FeatureOrder from "../FeatureOrder/FeatureOrder";
 import Featured from "../Featured/Featured";
@@ -7,6 +8,10 @@ import Testimonials from "../Testimonials/Testimonials";
 const Home = () => {
   return (
     <div>
+      <Helmet>
+        <meta charSet="utf-8" />
+        <title>Bistro || Home</title>
+      </Helmet>
       <Banner />
       <FeatureOrder />
       <PopularMenu />
--- src/pages/Menu/Menu/Menu.jsx
+++ src/pages/Menu/Menu/Menu.jsx
@@ -1,8 +1,12 @@
 import MenuHeading from "../../shared/MenuHeading/MenuHeading";
 import menuImage from "../../../assets/menu/banner3.jpg";
+import { Helmet } from "react-helmet";
 const Menu = () => {
   return (
     <div>
+      <Helmet>
+        <title>Bistro || Menu</title>
+      </Helmet>
       <MenuHeading
         image={menuImage}
         title="Our Menu"
